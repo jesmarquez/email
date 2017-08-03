97f9db7d7de0f79c362e039476ef10f7f6a9adae
Jesus Marquez <jesmqz@gmail.com>
2017-08-02 22:07:39 -0500

    adding send email - configuration

--- server.js
+++ server.js
@@ -2,11 +2,35 @@
 
 const express = require('express')
 const next = require('next')
+const smtpTransport = require('nodemailer-smtp-transport')
+const nodemailer = require('nodemailer')
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 let server
+
+// If EMAIL_USERNAME and EMAIL_PASSWORD are configured use them to send email.
+// e.g. For a Google Mail account (@gmail.com) set EMAIL_SERVICE to 'gmail'
+// See nodemailer documentation for other values for EMAIL_SERVICE.
+let mailserver = null
+
+const EMAIL_SERVER = 'smtp.gmail.com'
+const EMAIL_PORT = 465
+const EMAIL_SECURE = true
+const EMAIL_USERNAME = 'jesmqz@gmail.com'
+const EMAIL_PASSWORD = 'j3Z@7!6s'
+
+mailserver = smtpTransport({
+  host: EMAIL_SERVER,
+  port: EMAIL_PORT,
+  secure: EMAIL_SECURE,
+  auth: {
+    user: EMAIL_USERNAME,
+    pass: EMAIL_PASSWORD
+  }
+})
+
 
 app.prepare().then(()=> {
   server = express()
@@ -16,10 +40,10 @@ app.prepare().then(()=> {
     //res.status(200).send('POST send email')
 
     sendVerificationEmail({
-      mailserver: 'smtp.gmail.com',
+      mailserver: mailserver,
       fromEmail: 'noreply@localhost.localdomain',
       toEmail: 'jesmqz@gmail.com',
-      url: verificationUrl
+      url: 'verificationUrl'
     })
     return app.render(req, res, '/check-email')
   })
